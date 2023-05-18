aabed7c993b03e918aaf091bc5ae16d94ee89df8
Coordinacion de Desarrollo <89142648+CDSysdatec01@users.noreply.github.com>
2023-05-18 09:36:34 -0500

    update

--- Main.py
+++ Main.py
@@ -1,10 +1,19 @@
+from flask_cors import CORS
 from flask import Flask
 from Class import app as class_app
 from Dependence import app as deps_app
 from Month import app as month_app
+import configparser
 from Week import app as week_app
 
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+origins = config.get('CORS', 'origins')
+
 app = Flask(__name__)
+
+CORS(app, origins=origins)
 
 # Registro de las rutas para cada uno de los servicios
 app.register_blueprint(class_app, url_prefix='/Class')
